7c6e52551f705828480fbcf628ac9c6cb16b820b
Kevin Kwon <k@hellomarket.com>
2021-11-23 13:05:57 +0900

    init project

---
+++ feedforpad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  feedforpad
+//
+//  Created by kevin on 2021/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
